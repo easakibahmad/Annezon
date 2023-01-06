495137eccd3c16dec1461c1f9f9ea5ab2eda64ce
easakibahmad <sakib.sgc.du@gmail.com>
2023-01-06 11:16:07 +0600

    initial setup done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+function App() {
+  return (
+    <div>
+      <Button variant="contained" color="info">
+        Success
+      </Button>
+    </div>
+  );
+}
+
+export default App;
